664cd605f5d69a8ecb46d7a845b9e51228b8cb19
CarloCattano <freevibes@gmail.com>
2024-03-18 11:50:04 +0100

    show docker url on startup

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
 service nginx start 
 make run -C /home/root/webserv  &
+
 echo ""
+
+
+echo "http://"$(ip a | grep eth0 | grep inet | awk '{print $2}' | head -n1 | cut -d '/' -f 1):80 " - NGINX " 
+echo "http://"$(ip a | grep eth0 | grep inet | awk '{print $2}' | head -n1 | cut -d '/' -f 1):4242 " - WEBSERV "
+
 
 exec /bin/bash
